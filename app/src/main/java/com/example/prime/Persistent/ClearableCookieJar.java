5ee71928b027f7e3d172d75116711e88da2ccac8
srlord2000 <caridajhen@gmail.com>
2019-10-02 11:23:32 +0800

    try

---
+++ app/src/main/java/com/example/prime/Persistent/ClearableCookieJar.java
@@ -0,0 +1,16 @@
+package com.example.prime.Persistent;
+
+import okhttp3.CookieJar;
+
+public interface ClearableCookieJar extends CookieJar {
+
+    /**
+     * Clear all the session cookies while maintaining the persisted ones.
+     */
+    void clearSession();
+
+    /**
+     * Clear all the cookies from persistence and from the cache.
+     */
+    void clear();
+}

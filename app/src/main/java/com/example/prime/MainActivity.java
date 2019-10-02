cc2dcd9bb7a68a45b67d0c1729fdb1886ea6d32a
Ezly2406 <jlazarooooo@gmail.com>
2019-10-02 11:31:41 +0800

    upak ka ulit

--- app/src/main/java/com/example/prime/MainActivity.java
+++ app/src/main/java/com/example/prime/MainActivity.java
@@ -2,14 +2,50 @@ package com.example.prime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name="2131wqeq";
+        Button btn = findViewById(R.id.btnlogin);
+        Button forgot = findViewById(R.id.btnforgot);
+//        forgot.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(),Activity_Forgotpassword.class);
+//                startActivity(intent);
+//                overridePendingTransition( R.anim.slide_in_right, R.anim.slide_out_left);
+//            }
+//        });
+        Rellay();
+
+
+
+    }
+
+    public void Rellay(){
+
+        rellay1 =  findViewById(R.id.rellay1);
+        rellay2 =  findViewById(R.id.rellay2);
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+
     }
 }

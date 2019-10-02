512d0c88ed59205340f95f193f5c372853c6a78c
Ezly2406 <jlazarooooo@gmail.com>
2019-10-02 11:21:44 +0800

    upak ka

--- app/src/main/java/com/example/prime/MainActivity.java
+++ app/src/main/java/com/example/prime/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name="name4";
+        String name="2131wqeq";
     }
 }

9054395db3b9cde77604ee7410fabab059cd75d2
Ezly2406 <jlazarooooo@gmail.com>
2019-10-02 11:53:26 +0800

    problema nga ito

--- app/src/main/java/com/example/prime/MainActivity.java
+++ app/src/main/java/com/example/prime/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.prime.Model.MyModel;
 import com.example.prime.Persistent.ClearableCookieJar;
@@ -37,6 +39,16 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    RelativeLayout rellay1, rellay2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
     private Button btn,btn2;
     public static WebView wb;
     private String URLline = "http://192.168.0.100/auth/check";
@@ -50,82 +62,90 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sharedPrefsCookiePersistor = new SharedPrefsCookiePersistor(this);
-        apiInterface = ApiClientBuilder.getClient().create(ApiClient.class);
+        Rellay();
+//        sharedPrefsCookiePersistor = new SharedPrefsCookiePersistor(this);
+//        apiInterface = ApiClientBuilder.getClient().create(ApiClient.class);
 
-        List<Cookie> list=sharedPrefsCookiePersistor.loadAll();
-        for(int i = 0; i<list.size();i++){
-            Log.e(TAG, "onCreate: "+new Date(list.get(i).expiresAt()));
-            date = list.get(i).expiresAt();
-            id = list.get(i).value();
-        }
-        long dat2= (long) Long.parseLong(String.valueOf(new Date().getTime()));
-        if(date <= dat2){
-            sharedPrefsCookiePersistor.clear();
-        }
-        if(sharedPrefsCookiePersistor.loadAll().isEmpty()){
-            Load();
-        }
+//        List<Cookie> list=sharedPrefsCookiePersistor.loadAll();
+//        for(int i = 0; i<list.size();i++){
+//            Log.e(TAG, "onCreate: "+new Date(list.get(i).expiresAt()));
+//            date = list.get(i).expiresAt();
+//            id = list.get(i).value();
+//        }
+//        long dat2= (long) Long.parseLong(String.valueOf(new Date().getTime()));
+//        if(date <= dat2){
+//            sharedPrefsCookiePersistor.clear();
+//        }
+//        if(sharedPrefsCookiePersistor.loadAll().isEmpty()){
+//            Load();
+//        }
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                retrofit2.Call<ResponseBody> call = apiInterface.getJSONData1("ci_session="+id);
-                call.enqueue(new  retrofit2.Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse( retrofit2.Call<ResponseBody> call, Response<ResponseBody> response) {
-                        Log.e(TAG, "onResponse: "+response );
-                    }
-
-                    @Override
-                    public void onFailure( retrofit2.Call<ResponseBody> call, Throwable t) {
-                        Log.d(TAG, "onFailure: ");
-
-                    }
-                });
-
-            }
-        });
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                retrofit2.Call<ResponseBody> call = apiInterface.getJSONData1("ci_session="+id);
+//                call.enqueue(new  retrofit2.Callback<ResponseBody>() {
+//                    @Override
+//                    public void onResponse( retrofit2.Call<ResponseBody> call, Response<ResponseBody> response) {
+//                        Log.e(TAG, "onResponse: "+response );
+//                    }
+//
+//                    @Override
+//                    public void onFailure( retrofit2.Call<ResponseBody> call, Throwable t) {
+//                        Log.d(TAG, "onFailure: ");
+//
+//                    }
+//                });
+//
+//            }
+//        });
 
     }
 
-    private void Load(){
-        String user="admin",pass="secret";
-        String postData = null;
+//    private void Load(){
+//        String user="admin",pass="secret";
+//        String postData = null;
+//
+//        JSONObject jsonObject = new JSONObject();
+//        try {
+//            jsonObject.put("username","admin");
+//            jsonObject.put("password","secret");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        apiInterface = ApiClientBuilder.getClient().create(ApiClient.class);
+//        ClearableCookieJar cookieJar =
+//                new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(getApplicationContext()));
+//
+//        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .cookieJar(cookieJar)
+//                .build();
+//
+//        RequestBody body = RequestBody.create(String.valueOf(jsonObject), JSON);
+//
+//        okhttp3.Request request = new okhttp3.Request.Builder()
+//                .url("http://192.168.0.100/auth/check")
+//                .post(body)
+//                .build();
+//
+//        okHttpClient.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(okhttp3.Call call, IOException e) {
+//                call.cancel();
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+//                Log.d("TAG", response.body().string());
+//                Log.d("TAG", response.headers().toString());
+//            }
+//        });
+//    }
+    public void Rellay(){
 
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("username","admin");
-            jsonObject.put("password","secret");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        apiInterface = ApiClientBuilder.getClient().create(ApiClient.class);
-        ClearableCookieJar cookieJar =
-                new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(getApplicationContext()));
+        rellay1 =  findViewById(R.id.rellay1);
+        rellay2 =  findViewById(R.id.rellay2);
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
 
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .cookieJar(cookieJar)
-                .build();
-
-        RequestBody body = RequestBody.create(String.valueOf(jsonObject), JSON);
-
-        okhttp3.Request request = new okhttp3.Request.Builder()
-                .url("http://192.168.0.100/auth/check")
-                .post(body)
-                .build();
-
-        okHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-                call.cancel();
-            }
-
-            @Override
-            public void onResponse(Call call, okhttp3.Response response) throws IOException {
-                Log.d("TAG", response.body().string());
-                Log.d("TAG", response.headers().toString());
-            }
-        });
     }
 }

f4bc846c049f0d97d60167c7e42379e41960322e
srlord2000 <caridajhen@gmail.com>
2019-10-04 14:45:09 +0800

    try

--- app/src/main/java/com/example/prime/HomeActivity.java
+++ app/src/main/java/com/example/prime/HomeActivity.java
@@ -11,14 +11,11 @@ import com.example.prime.Fragment.SettingsFragment;
 import com.example.prime.Fragment.StaffSettingsFragment;
 import com.example.prime.Fragment.StationsFragment;
 import com.example.prime.Persistent.SharedPrefsCookiePersistor;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
 
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
+import android.os.Handler;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
@@ -47,15 +44,7 @@ public class HomeActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         sharedPrefsCookiePersistor = new SharedPrefsCookiePersistor(this);
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        editor = prefs.edit();
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,6 +56,7 @@ public class HomeActivity extends AppCompatActivity
         navigationView.setCheckedItem(R.id.nav_monitoring);
         Fragment fragment = new HomeFragment();
         displaySelectedFragment(fragment);
+
 
         Menu nav_Menu = navigationView.getMenu();
         if(prefs.getString("level","").equals("1")){
@@ -117,46 +107,52 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        Fragment fragment = null;
-        if (id == R.id.nav_monitoring) {
+        final int id = item.getItemId();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
 
-            fragment = new HomeFragment();
-            displaySelectedFragment(fragment);
+                Fragment fragment = null;
+                if (id == R.id.nav_monitoring) {
 
-        } else if (id == R.id.nav_reports) {
+                    fragment = new HomeFragment();
+                    displaySelectedFragment(fragment);
 
-            fragment = new ReportFragment();
-            displaySelectedFragment(fragment);
+                } else if (id == R.id.nav_reports) {
 
-        } else if (id == R.id.nav_stations) {
+                    fragment = new ReportFragment();
+                    displaySelectedFragment(fragment);
 
-            fragment = new StationsFragment();
-            displaySelectedFragment(fragment);
+                } else if (id == R.id.nav_stations) {
 
-        } else if (id == R.id.nav_inventory) {
+                    fragment = new StationsFragment();
+                    displaySelectedFragment(fragment);
 
-            fragment = new InventoryFragment();
-            displaySelectedFragment(fragment);
+                } else if (id == R.id.nav_inventory) {
 
-        } else if (id == R.id.nav_settings) {
-            if(prefs.getString("level","").equals("1")) {
-                fragment = new SettingsFragment();
-                displaySelectedFragment(fragment);
-            }else{
-                fragment = new StaffSettingsFragment();
-                displaySelectedFragment(fragment);
+                    fragment = new InventoryFragment();
+                    displaySelectedFragment(fragment);
+
+                } else if (id == R.id.nav_settings) {
+                    if (prefs.getString("level", "").equals("1")) {
+                        fragment = new SettingsFragment();
+                        displaySelectedFragment(fragment);
+                    } else {
+                        fragment = new StaffSettingsFragment();
+                        displaySelectedFragment(fragment);
+                    }
+
+                } else if (id == R.id.nav_logout) {
+                    editor = prefs.edit();
+                    Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                    sharedPrefsCookiePersistor.clear();
+                    editor.clear();
+                    editor.apply();
+                    startActivity(intent);
+                    finish();
+                }
             }
-
-        } else if (id == R.id.nav_logout) {
-
-            Intent intent = new Intent(HomeActivity.this,MainActivity.class);
-            sharedPrefsCookiePersistor.clear();
-            editor.clear();
-            startActivity(intent);
-            finish();
-        }
-
+        }, 10);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;

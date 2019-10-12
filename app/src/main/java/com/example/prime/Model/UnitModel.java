d62958348d84eac1fe8af322e2515aa0ebf3690b
srlord2000 <caridajhen@gmail.com>
2019-10-12 09:52:52 +0800

    try

---
+++ app/src/main/java/com/example/prime/Model/UnitModel.java
@@ -0,0 +1,62 @@
+package com.example.prime.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UnitModel {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("unit_name")
+    @Expose
+    private String unitName;
+    @SerializedName("group_id")
+    @Expose
+    private String groupId;
+    @SerializedName("time_added")
+    @Expose
+    private String timeAdded;
+
+    public UnitModel(String id, String unitName, String groupId, String timeAdded) {
+        this.id = id;
+        this.unitName = unitName;
+        this.groupId = groupId;
+        this.timeAdded = timeAdded;
+    }
+
+    public UnitModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getTimeAdded() {
+        return timeAdded;
+    }
+
+    public void setTimeAdded(String timeAdded) {
+        this.timeAdded = timeAdded;
+    }
+
+}

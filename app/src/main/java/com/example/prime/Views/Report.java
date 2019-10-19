ecfa4f76d322f1cdbfa76fef9689507785b46c34
Ezly2406 <jlazarooooo@gmail.com>
2019-10-19 16:32:59 +0800

    problema nga ito

--- app/src/main/java/com/example/prime/Views/Report.java
+++ app/src/main/java/com/example/prime/Views/Report.java
@@ -1,30 +1,59 @@
 package com.example.prime.Views;
 
 import android.app.DatePickerDialog;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.prime.R;
 
+import java.io.File;
 import java.util.Calendar;
 
 public class Report extends Fragment {
     Context mContext;
-    Button dateButton;
+    Button dateButton,downloadButton;
+    private long downloadID;
+
     final Calendar newCalendar = Calendar.getInstance();
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 //        mContext = context;
     }
+
+    private BroadcastReceiver onDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Fetching the download id received with the broadcast
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            //Checking if the received broadcast is for our enqueued download by matching download id
+            if (downloadID == id) {
+                Toast.makeText(mContext, "Download Completed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().unregisterReceiver(onDownloadComplete);
+    }
+
+
 
 
     @Override
@@ -38,6 +67,10 @@ public class Report extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         dateButton = view.findViewById(R.id.dateReport);
+        downloadButton = view.findViewById(R.id.btnDownload);
+        getActivity().registerReceiver(onDownloadComplete,new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+
 
         final DatePickerDialog StartTime = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -52,7 +85,35 @@ public class Report extends Fragment {
                 StartTime.show();
             }
         });
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               beginDownload();
+
+            }
+        });
     }
+
+
+
+    private void beginDownload(){
+        File file =new File(getActivity().getExternalFilesDir(null),"Dummy");
+        /*
+        Create a DownloadManager.Request with all the information necessary to start the download
+         */
+        DownloadManager.Request request=new DownloadManager.Request(Uri.parse("http://ipv4.download.thinkbroadband.com/5MB.zip"))
+                .setTitle("Dummy File")// Title of the Download Notification
+                .setDescription("Downloading")// Description of the Download Notification
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)// Visibility of the download Notification
+                .setDestinationUri(Uri.fromFile(file))// Uri of the destination file
+                .setRequiresCharging(false)// Set if charging is required to begin the download
+                .setAllowedOverMetered(true)// Set if download is allowed on Mobile network
+                .setAllowedOverRoaming(true);// Set if download is allowed on roaming network
+        DownloadManager downloadManager= (DownloadManager)getActivity().getBaseContext().getSystemService(Context.DOWNLOAD_SERVICE);
+        downloadID = downloadManager.enqueue(request);// enqueue puts the download request in the queue.
+    }
+
 }
 
 

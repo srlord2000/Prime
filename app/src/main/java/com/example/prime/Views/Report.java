9abda387e2f1107dd46e1800268ca0df5242758d
Ezly2406 <jlazarooooo@gmail.com>
2019-10-23 16:02:58 +0800

    problema nga ito

--- app/src/main/java/com/example/prime/Views/Report.java
+++ app/src/main/java/com/example/prime/Views/Report.java
@@ -178,7 +178,7 @@ public class Report extends Fragment {
     private boolean writeResponseBodyToDisk(ResponseBody body) {
         try {
             // todo change the file location/name according to your needs
-            File futureStudioIconFile = new File(getActivity().getExternalFilesDir(null) ,"DUMMY");
+            File futureStudioIconFile = new File(getActivity().getExternalFilesDir(null) + File.separator + "EDIWOW.doc");
 
             InputStream inputStream = null;
             OutputStream outputStream = null;
